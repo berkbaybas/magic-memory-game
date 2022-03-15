98af4d3941e85d0a56217fc45a487aa217b413c2
berk <berk_baybas@hotmail.com>
2022-03-15 21:11:09 +0300

    first view css bug fixed

--- src/styles/MainContainer.styles.js
+++ src/styles/MainContainer.styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 const MainContainer = styled.div`
   height: 100%;
+  min-height: calc(100vh - 64px);
   background-color: #6f1e38;
   padding-top: 32px;
   padding-bottom: 32px;
